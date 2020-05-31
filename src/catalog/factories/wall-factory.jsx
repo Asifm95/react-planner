3dff322ccfb2a4fafc59a29b3c5268e8bd1ae927
Asif Mohammed <asifm2882@gmail.com>
2020-05-31 21:02:51 +0530

    reduce wall thinckness

--- src/catalog/factories/wall-factory.jsx
+++ src/catalog/factories/wall-factory.jsx
@@ -30,7 +30,7 @@ export default function WallFactory(name, info, textures) {
         label: translator.t('thickness'),
         type: 'length-measure',
         defaultValue: {
-          length: 20
+          length: 5
         }
       }
     },
